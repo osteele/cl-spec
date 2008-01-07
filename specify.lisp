1531f76ca16839ef786d8a05740cb14e826f81a7
Oliver Steele <steele@osteele.com>
2008-01-07 11:36:05 -0500

    formatted output

--- specify.lisp
+++ specify.lisp
@@ -1,18 +1,28 @@
-(define-class specification name setup tests)
+(define-class specification name setup examples)
 
 (define-print-method (specification name) "#<specification ~S>" name)
 
-(define-method (run (self specification))
+(define-method (run-specification (self specification) &key onsuccess onerror)
   (flet ((run-example (name fn)
            (if (specification-setup self)
                (funcall (specification-setup self)))
-           (handler-case (funcall fn)
+           (handler-case (progn
+                           (funcall fn)
+                           (if onsuccess
+                               (funcall onsuccess name))
+                           t)
              (expectation-not-met (condition)
-               (format t "~A while executing ~A" condition name)))))
-    (loop for (name . fn) in (specification-tests self)
-       do (run-example name fn))))
+               ;(format t "~A while executing ~A" condition name)
+               (if onerror
+                   (funcall onerror name condition))
+               nil))))
+    (loop for (name . fn) in (specification-examples self)
+       collect (cons name (run-example name fn)))))
 
-;; TODO: run initializers on preamble
+(defvar *collect-specifications* nil)
+(defvar *run-specifications* t)
+(defvar *specifications* nil)
+
 (defmacro define-specification (name values &body body)
   (let* ((before nil)
          (specs
@@ -21,7 +31,7 @@
              collect item
              else if (and (eq (car item) 'before) (eq (cadr item) :each))
              do (setf before (cddr item))))
-         (tests
+         (examples
           (loop for item in specs
              collect ``(,,(car item) . ,#'(lambda () ,@(cdr item))))))
     `(let ,values
@@ -29,7 +39,39 @@
               (make-instance 'specification
                              :name ,name
                              :setup #'(lambda() ,@before)
-                             :tests (list ,@tests))))
-         (run spec)
-         (setq *spec* spec)
-         spec))))
+                             :examples (list ,@examples))))
+         (if *collect-specifications*
+             (push spec *specifications*))
+         (if *run-specifications*
+             (run-specification spec)
+             spec)))))
+
+(define-method (run-specification (pathname pathname) &key onsuccess onerror)
+  (declare (ignore onsuccess onerror))
+  (let ((*collect-specifications* t)
+        (*run-specifications* nil)
+        (*specifications* nil)
+        (t0 (get-internal-real-time))
+        (example-count 0)
+        (failures nil)
+        elapsed-time)
+    (load pathname)
+    (for spec in *specifications*
+      do (incf example-count (length (specification-examples spec)))
+      do (run-specification spec
+                            :onsuccess #'(lambda (name)
+                                           (declare (ignore name))
+                                           (format t "."))
+                            :onerror #'(lambda (name condition)
+                                         (push `(,name . ,condition) failures)
+                                         (format t "F"))))
+    (setf elapsed-time (- (get-internal-real-time) t0))
+    (format t "~%~%")
+    ;(print failures)
+    (loop for (name . condition) in failures
+         for i upfrom 1
+         do (format t "~D)~%~A~%~A~%~%" i condition pathname))
+    (format t "Finished in ~F seconds~%~%"
+            (/ elapsed-time INTERNAL-TIME-UNITS-PER-SECOND))
+    (format t "~D example~:P, ~D failure~:P" example-count (length failures))
+    (values)))
--- specs/fail-spec.lisp
+++ specs/fail-spec.lisp
@@ -1,3 +1,5 @@
 (define-specification "failure" ()
+  ("should pass"
+   (=> (+ 1 2) should = 3))
   ("should fail"
    (=> (+ 1 2) should = 4)))
